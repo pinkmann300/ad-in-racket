72e1cfc4edad9026ed07d1b3b1bf169df795bc74
SandeepC <sandeepchezhian@gmail.com>
2023-09-24 16:48:16 +0530

    Removed auto_diff.hs

--- taylor_num.rkt
+++ taylor_num.rkt
@@ -52,6 +52,12 @@
       
 (check-within ((dfn 0) (Taylor 5.0 (Taylor 2.0 (Zero)))) 5.0 0.0001)
 
+(: x (-> Flonum TaylorZ))
+(define
+  (x a)
+  (Taylor a 1.0))
+
+
 (: teval (-> Integer TaylorZ Flonum Flonum))
 (define (teval k t x)
   (match (list k t x)
@@ -63,10 +69,6 @@
       (fl/
        (fl* ((dfn k) t) (flexpt x (->fl k)))
        (->fl (factorial k))))]))
-
-(struct (t) Num
-  ([add : (-> t t t)])
-  #:transparent)
 
 
 (: TaylorAdd (-> TaylorZ TaylorZ TaylorZ))
@@ -82,27 +84,108 @@
 (define
   (TaylorMul [a : TaylorZ] [b : TaylorZ])
      (match (list a b)
-       [(list (Zero) b) (Zero)]
-       [(list a (Zero)) (Zero)]
+       [(list (Zero) b) 0.0]
+       [(list a (Zero)) 0.0]
        [(list a b) (Taylor (fl* (f a) (f b))
                            (TaylorAdd
                             (TaylorMul (df a) b)
-                            (TaylorMul (df b) a)))]))
-
+                            (TaylorMul (df b) a)))])
 
 (: TaylorNegate (-> TaylorZ TaylorZ))
 (define
   (TaylorNegate [a : TaylorZ])
      (match a
-       [(Zero) (Zero)]
+       [(Zero) 0.0]
        [a  (Taylor (fl* -1.0 (f a))
                    (TaylorNegate (df a)))]))
-#|
+
 (: TaylorSignum (-> TaylorZ TaylorZ))
 (define
   (TaylorSignum [a : TaylorZ])
      (match a
-       [(Zero) (Zero)]
-       [a (Taylor (signum 
-|#
+       [(Zero) 0.0]
+       [a (Taylor (sgn (f a))
+                  (TaylorSignum (df a)))]))
 
+(: TaylorAbsol (-> TaylorZ TaylorZ))
+(define
+  (TaylorAbsol [a : TaylorZ])
+     (match a
+       [(Zero) 0.0]
+       [a (Taylor (sgn (f a))
+                  0.0)]))
+
+(: TaylorFromFloat (-> Flonum TaylorZ))
+(define
+  (TaylorFromFloat [a : Flonum])
+     (match a
+       [0.0 (Zero)]
+       [a (Taylor a 0.0)]))
+
+
+(struct (t) Num
+  ([add : (-> t t t)]
+   [mul : (-> t t t)]
+   [neg : (-> t t)]
+   [absol : (-> t t)]
+   [signum : (-> t t)]
+   [fromFloat : (-> Float t)])
+  #:transparent)
+
+
+; Instance declaration for the TaylorNum to be a part of the Num typeclass
+
+(define TaylorNum
+  (Num
+   (λ ([a : TaylorZ] [b : TaylorZ])
+     (TaylorAdd a b))
+   (λ ([a : TaylorZ] [b : TaylorZ])
+     (TaylorMul a b))
+   (λ ([a : TaylorZ])
+     (TaylorNegate a))
+   (λ ([a : TaylorZ])
+     (TaylorAbsol a))
+   (λ ([a : TaylorZ])
+     (TaylorSignum a))
+   (λ ([a : Flonum])
+     (TaylorFromFloat a))))
+
+(struct (t) Eq
+  ([equal : (-> t t Boolean)])
+  #:transparent)
+
+(define TaylorEq
+  (Eq
+   (λ ([a : TaylorZ][b : TaylorZ])
+     (eq? (f a) (f b)))))
+
+(struct (t) Ord
+  ([geq : (-> t t Boolean)])
+  #:transparent)
+
+(define TaylorOrd
+  (Ord
+   (λ ([a : TaylorZ][b : TaylorZ])
+     (<= (f a) (f b)))))
+
+;; Having some trouble introducing the fractional type because in haskell Fractional is a part of the Num class if im not wrong
+
+
+(struct (t) Floating
+  ([pi : (-> t)]
+   [exp : (-> t t)]
+   [log : (-> t t)]
+   [sin : (-> t t)]
+   [cos : (-> t t)]
+   [arcsin : (-> t t)]
+   [arccos : (-> t t)]
+   [arctan : (-> t t)]
+   [sinh : (-> t t)]
+   [cosh : (-> t t)]
+   [asinh : (-> t t)]
+   [acosh : (-> t t)]
+   [atanh : (-> t t)]))
+
+
+             
+                
